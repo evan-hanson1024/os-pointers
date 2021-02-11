80fc387107c7f215eec7009de020f3d261d0a2b8
Evan Hanson <hans2624@stthomas.edu>
2021-02-11 13:03:35 -0600

    Completed assignment. Average is calculated and cases for integers and doubles are checked.

--- pointers.cpp
+++ pointers.cpp
@@ -1,6 +1,7 @@
 // compile: g++ -std=c++11 -o pointers pointers.cpp
 #include <iostream>
 #include <string>
+#include <limits>
 
 typedef struct Student {
     int id;
@@ -20,9 +21,40 @@ int main(int argc, char **argv)
     double average;
 
     // Sequence of user input -> store in fields of `student`
+    student.id = promptInt("Please enter the student's id number: ", 0, 999999999);
+
+    student.f_name = new char[128];
+    std::cout << "Please enter the student's first name: ";
+    std::cin >> student.f_name;
+
+    student.l_name = new char[128];
+    std::cout << "Please enter the student's last name: ";
+    std::cin >> student.l_name;
+
+    student.n_assignments = promptInt("Please enter how many assignments were graded: ", 1, 134217728);
+
+
+    student.grades = new double[student.n_assignments];
+    int i;
+    std::cout << "\n";
+    std::string message = "Please enter grade for assignment ";
+    std::string suffix = ": ";
+    for (i = 0; i < student.n_assignments; i++) {
+        std::string index = std::to_string(i);
+        student.grades[i] = promptDouble(message + index + suffix, 0, 1000.0);
+    }
 
     // Call `CalculateStudentAverage(???, ???)`
+    calculateStudentAverage(&student, &average);
+    //Round average
+    average = (double)((int)((average * 10) + 0.5))/10;
+
+    //Set up strings for output
+    std::string output_name_id = std::string("\n") + std::string("Student: ") + std::string(student.f_name) + std::string(" ") + std::string(student.l_name) + std::string(" ") + std::string("[") + std::to_string(student.id) + std::string("]\n");
+
     // Output `average`
+    std::cout << output_name_id;
+    std::cout << "  Average grade: " << average << "\n";
 
     return 0;
 }
@@ -34,7 +66,36 @@ int main(int argc, char **argv)
 */
 int promptInt(std::string message, int min, int max)
 {
-    // Code to prompt user for an int
+    int candidate;
+    bool is_not_int_flag = false;
+    std::string candidate_string;
+
+    while(true){
+        is_not_int_flag = false;
+        std::cout << message;
+        std::cin >> candidate_string;
+
+        int i;
+        for (i = 0; i < candidate_string.length(); i++) {
+            if (isdigit(candidate_string[i]) == false) {
+                is_not_int_flag = true;
+            }
+        }
+
+        if (!is_not_int_flag) {
+            candidate = std::stoi(candidate_string);
+        }
+
+        if(candidate > min && candidate < max && !is_not_int_flag){
+            break;
+        }
+        else{
+            std::cout << "Sorry, I cannot understand your answer" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    }
+    return candidate;
 }
 
 /*
@@ -44,7 +105,22 @@ int promptInt(std::string message, int min, int max)
 */
 double promptDouble(std::string message, double min, double max)
 {
-    // Code to prompt user for a double
+    double candidate;
+
+    while(true){
+        std::cout << message;
+        std::cin >> candidate;
+
+        if(candidate > min && candidate < max){
+            break;
+        }
+        else{
+            std::cout << "Sorry, I cannot understand your answer" << std::endl;
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
+    }
+    return candidate;
 }
 
 /*
@@ -54,4 +130,13 @@ double promptDouble(std::string message, double min, double max)
 void calculateStudentAverage(void *object, double *avg)
 {
     // Code to calculate and store average grade
+    Student* student = (Student*)object;
+    int number_assignments = student->n_assignments;
+    int i = 0;
+    double currentSum = 0;
+    for (int i = 0; i < number_assignments; i++) {
+        currentSum = currentSum + student->grades[i];
+    }
+    *avg = currentSum/number_assignments;
+
 }
